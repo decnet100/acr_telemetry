6c42935e8646a2e372bdbe9f51b561e9743d850a
decnet100 <59013021+decnet100@users.noreply.github.com>
2026-02-23 22:05:48 +0100

    Clarify stopping mechanism in acr_stop.bat
    Updated comment to clarify the stopping mechanism for acr_recorder.

--- acr_stop.bat
+++ acr_stop.bat
@@ -1,4 +1,4 @@
 @echo off
-:: Signals acr_recorder to stop. Bind this to a game controller button for in-game stop.
+:: Signals acr_recorder to stop by creating an empty file in a directory it watches. Binding this to a game controller button for in-game stop is a lot more efficient than listening for a hotkey.
 mkdir "%APPDATA%\acr_recorder" 2>nul
 echo. > "%APPDATA%\acr_recorder\acr_stop"
