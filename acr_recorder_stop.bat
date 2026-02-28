a3a20db20980d2fc8ce637e26ceb9bbb78aa1f3a
decnet100 <chdem100@gmail.com>
2026-02-28 09:03:18 +0100

    gitignore temp fix

--- acr_recorder_stop.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-:: Creates stop file in %APPDATA%\acr_telemetry\ (e.g. C:\Users\<user>\AppData\Roaming\acr_telemetry\)
-mkdir "%APPDATA%\acr_telemetry" 2>nul
-echo. > "%APPDATA%\acr_telemetry\acr_stop"
